309acbd73a12120bc51df7e430bb809ca6339219
James Swatton <jamessswatton@gmail.com>
2019-04-05 13:28:58 +0100

    add increase_pets_sold func

--- pet_shop.rb
+++ pet_shop.rb
@@ -12,6 +12,12 @@ def add_or_remove_cash(petshop, cash)
   petshop[:admin][:total_cash] += cash
 end
 
+
 def pets_sold(petshop)
   return petshop[:admin][:pets_sold]
+end
+
+
+def increase_pets_sold(petshop, amount_of_pets_sold)
+  petshop[:admin][:pets_sold] += amount_of_pets_sold
 end
